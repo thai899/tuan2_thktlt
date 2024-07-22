5a1e67f896909ac0479e20d8e5d66abcc0f41a59
Thai <trinhhoangthai2005@gmail.com>
2024-07-22 19:42:48 +0700

    so sanh 2 hon so

--- code/bai2.cpp
+++ code/bai2.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<conio.h>
 #include <stdlib.h>
 #include<time.h>
@@ -33,12 +33,21 @@ HonSo taoHonSoNgauNhien() {
 void xuatHonSo(HonSo hs) {
 	printf("%d %d/%d\n", hs.phanNguyen, hs.tuSo, hs.mauSo);
 }
+int soSanhHonSo(HonSo hs1, HonSo hs2) {
+	double giaTri1 = hs1.phanNguyen + (double)hs1.tuSo / hs1.mauSo;
+	double giaTri2 = hs2.phanNguyen + (double)hs2.tuSo / hs2.mauSo;
+	if (giaTri1 < giaTri2) return -1;
+	if (giaTri1 > giaTri2) return 1;
+	return 0;
+}
 int main()
 {
 	int lc;
 	srand(time(NULL));
 	int n = 5;
 	HonSo b[5];
+	HonSo hs1 = b[0];
+	HonSo hs2 = b[1];
 	do {
 		menu();
 		printf("\nMoi ban chon ");
@@ -62,7 +71,26 @@ int main()
 			}
 		}break;
 		case 3:
-		{}break;
+		{
+			HonSo hs1 = taoHonSoNgauNhien();
+			HonSo hs2 = taoHonSoNgauNhien();
+
+			printf("Hỗn số 1: ");
+			xuatHonSo(hs1);
+			printf("Hỗn số 2: ");
+			xuatHonSo(hs2);
+
+			int ketQua = soSanhHonSo(hs1, hs2);
+			if (ketQua < 0) {
+				printf("Hon so 1 nho hơn hon so 2\n");
+			}
+			else if (ketQua > 0) {
+				printf("Hon so 1 lon hon hon so 2\n");
+			}
+			else {
+				printf("Hon so 1 bang hon so 2\n");
+			}
+		}break;
 		case 4:
 		{}break;
 		case 5:
@@ -73,4 +101,5 @@ int main()
 		{}break;
 		}
 	} while(lc<8);
+	return 0;
 }
